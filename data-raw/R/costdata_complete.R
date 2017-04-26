e560afe4c484f3545acb7eb4a3df779c33d97233
iriseekhout <iriseekhout@gmail.com>
2017-04-26 10:08:32 +0200

    Remove data files

--- data-raw/R/costdata_complete.R
+++
@@ -1,9 +0,0 @@
-library(foreign)
-
-dat <-read.spss("~/CloudStation/R projects/Functions & Programs/Missing data/Makemissing/data-raw/Hanneke van Dongen/20161111_PROBAATDATA_CLEAN.sav",to.data.frame = TRUE)
-
-head(dat)
-
-costdata_complete <- dat
-
-save("costdata_complete", file = "~/CloudStation/R projects/Functions & Programs/Missing data/Makemissing/data/costdata_complete.rda",compress = "xz")
